8d5d1b69efc22f9a1b07dadcccb44a78827a6de4
andresugartechea <78426894+andresugartechea@users.noreply.github.com>
2022-04-28 11:59:29 +0400

    sqsq

--- public/script.js
+++ public/script.js
@@ -5,13 +5,43 @@ socket.on('connect', () => {
 
 })
 
-window.addEventListener('load', () => {
-    // let submitButton = document.getElementById('send-button');
+// window.addEventListener('load', () => {
+//     // let submitButton = document.getElementById('send-button');
 
-    // submitButton.addEventListener('click', () => {
-    //     let name = document.getElementById('name-input').value;
-    //     let msg = document.getElementById('msg-input').value;
-    //     console.log(name, msg);
-    // })
+//     // submitButton.addEventListener('click', () => {
+//     //     let name = document.getElementById('name-input').value;
+//     //     let msg = document.getElementById('msg-input').value;
+//     //     console.log(name, msg);
+//     // })
 
-})
+// })
+
+
+////ML5
+//console.log('ml5 version:', ml5.version);
+
+let img;
+let detector;
+
+function preload(){
+    img = loadImage("/img/cat.jpg");
+    detector = ml5.objectDetector('cocossd');
+}
+
+function gotDetections(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function setup() {
+    createCanvas(1000, 1000);
+    //console.log(detector);
+    image(img, 0, 0);
+    detector.detect(img, gotDetections);
+}
+
+function draw() {
+
+}
