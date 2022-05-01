9339bd6d30d305ac16e0c7cbb7dc5f2459341fa0
andresugartechea <78426894+andresugartechea@users.noreply.github.com>
2022-05-01 16:20:31 +0400

    lme

--- public/app.js
+++ public/app.js
@@ -7,7 +7,6 @@ socket.on('connect', () => {
 })
 
 
-
 // // Your code will go here
 // // open up your console - if everything loaded properly you should see the latest ml5 version
 // //console.log('ml5 version:', ml5.version);
@@ -31,21 +30,31 @@ function gotDetections(error, results) {
 }
 
 function setup() {
-  createCanvas(612, 408);
+  //to create canvas and put it in the background
+  canvas = createCanvas(windowWidth,windowHeight);
+  canvas.position(0, 0);
+  //canvas.style('z-index', '-1');
   //console.log(detector);
   //image(img, 0, 0);
   video = createCapture(VIDEO);
+  //video.size(height, height)
   video.size(640, 480);
   video.hide();
   detector.detect(video, gotDetections);
 }
 
+//to resixe window every time there's a change
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function draw() {
-  image(video, 0, 0);
+  background(0);
+  image(video, 0, 0)//, video.height, video.height);
 
   for (i in detections){
     let object = detections[i];
-    stroke(0, 255, 0);
+     stroke(0, 255, 0);
     strokeWeight(4);
     noFill();
     rect(object.x, object.y, object.width, object.height);
@@ -53,5 +62,13 @@ function draw() {
     fill(255);
     textSize(24);
     text(object.label, object.x + 10, object.y + 24);
+
+    if(object.label == "bottle"){
+      background(255, 255, 0);
+    }
+
+    if(object.label == "cell phone"){
+      background(255, 0, 0);
+    }
   }
 }
