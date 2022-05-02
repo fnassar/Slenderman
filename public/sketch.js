06f9e7177ef8137d4a23673ee9d3210388638e37
andresugartechea <78426894+andresugartechea@users.noreply.github.com>
2022-05-02 17:58:49 +0400

    In this part I use the phone's front camera for the object detection

--- public/sketch.js
+++ public/sketch.js
@@ -2,10 +2,15 @@
 // // open up your console - if everything loaded properly you should see the latest ml5 version
 // //console.log('ml5 version:', ml5.version);
 
-let img;
+
+//To change the camera https://editor.p5js.org/AndreasRef/sketches/HJVgGjmz4
+
+
 let video;
 let detector;
 let detections = [];
+
+let gameState;
 
 function preload() {
     img = loadImage("/img/cat.jpg");
@@ -21,33 +26,93 @@ function gotDetections(error, results) {
 }
 
 function setup() {
-    const myCanvas = createCanvas(612, 408);
-    myCanvas.parent("canvas_container");
-    //console.log(detector);
-    //image(img, 0, 0);
-    video = createCapture(VIDEO);
-    video.size(640, 480);
-    video.hide();
-    // error here no clue why
-    detector.detect(video, gotDetections);
+  //to create canvas and put it in the background
+  canvas = createCanvas(windowWidth,windowHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
+
+  //to set up the front camera on the phone
+  var constraints = {
+    audio: false,
+
+    ///TO TEST ON PHONE, COMMENT IN THIS PART AND COMMENT OUT THE OTHER ONE 
+    video: {
+      facingMode: {
+        exact: "environment"
+      }
+    }    
+    // video: {
+    //   facingMode: "user"
+    // } 
+  };
+  video = createCapture(constraints);
+  //video.size(windowHeight, 480);
+  video.hide();
+
+  detector.detect(video, gotDetections);
+
+  //To start game:
+  gameState = "instructions";
 }
+
+// // // //to resize window every time there's a change
+// function windowResized(){
+//     resizeCanvas(windowWidth, windowHeight);
+// }
 
 
 function draw() {
-    translate(width, 0); // move to far corner
-    scale(-1.0, 1.0); // flip x-axis backwards
-    image(video, 0, 0, width, height); //video on canvas, position, dimensions
+    // translate(width, 0); // move to far corner
+    // scale(-1.0, 1.0); // flip x-axis backwards
 
-    for (i in detections) {
-        let object = detections[i];
-        stroke(0, 255, 0);
-        strokeWeight(4);
-        noFill();
-        rect(object.x, object.y, object.width, object.height);
-        noStroke();
-        fill(255);
-        textSize(24);
-        text(object.label, object.x + 10, object.y + 24);
 
+    if (gameState == "start"){
+        background(255,0,0);
+        image(video, 0, 0, video.width, video.height); //video on canvas, position, dimensions
+
+        square(width-50, 0, 50)
+
+        for (i in detections) {
+            let object = detections[i];
+            stroke(0, 255, 0);
+            strokeWeight(4);
+            noFill();
+            rect(object.x, object.y, object.width, object.height);
+            noStroke();
+            fill(255);
+            textSize(24);
+            text(object.label, object.x + 10, object.y + 24);
+            
+            // if(object.label == "bottle"){
+            //     background(255, 255, 0);
+            // }
+    
+            // if(object.label == "cell phone"){
+            //     background(255, 0, 0);
+            // }
+        }
+    }
+    else if (gameState == "instructions"){
+        background(255, 255, 0);
+    }
+    else if(gameState == "help") {
+        rect(20, 20, width-40, height-40)
+    }
+
+}
+
+function touchStarted() {
+    if (gameState == "instructions") {
+        gameState = "start"
+    }
+    else if (gameState == "start") {
+        if((mouseX>width-50)&&(mouseY<50)){
+            gameState = "help"
+        }
+    }
+    else if (gameState == "help") {
+        if((mouseX>width-50)&&(mouseY<50)){
+            gameState = "start"
+        }
     }
 }
