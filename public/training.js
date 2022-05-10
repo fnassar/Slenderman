716269d34d78b9fb12a022840eff9b5186f27909
andresugartechea <78426894+andresugartechea@users.noreply.github.com>
2022-05-10 13:00:44 +0400

    trained model (good version)

--- public/training.js
+++ public/training.js
@@ -1,6 +1,6 @@
 // // Your code will go here
 // // open up your console - if everything loaded properly you should see the latest ml5 version
-// //console.log('ml5 version:', ml5.version);
+// //console.log('ml5 version:', ml5.version)
 
 
 //To change the camera https://editor.p5js.org/AndreasRef/sketches/HJVgGjmz4
@@ -32,6 +32,11 @@ function gotResults(error, results){
         console.error(error);
     }
     console.log(results)
+    if (results[0].confidence>0.90){
+        label = results[0].label;
+    } else {
+        label = "no object found"
+    }
     classifier.classify(gotResults);
 }
 
@@ -50,14 +55,14 @@ function videoReady() {
     console.log("video is ready")
 }
 
-// function whileTraining(loss){
-//     if(loss == null) {
-//         //console.log("training complete");
-//         fill(255);
-//         document.getElementById("title").innerHTML = "training complete"
-//         classifier.classify(gotResults)
-//     }
-// }
+function whileTraining(loss){
+    if(loss == null) {
+        //console.log("training complete");
+        fill(255);
+        document.getElementById("title").innerHTML = "training complete"
+        classifier.classify(gotResults)
+    }
+}
 
 function setup() {
     //to create canvas and put it in the background
@@ -85,64 +90,68 @@ function setup() {
 
     //COCOSD
     mobilenet = ml5.featureExtractor('MobileNet', modelReady);
-    classifier = mobilenet.classification(video_trained, videoReady);
+    classifier = mobilenet.classification(video_trained, {numLabels: 8} , videoReady);
 
 
-    // //To train ML5
-    // page1 = createButton('always watches');
-    // page1.mousePressed(function(){
-    //     classifier.addImage('always watches');
-    // });
+    //To train ML5
+    page1 = createButton('always watches');
+    page1.mousePressed(function(){
+        classifier.addImage('always watches');
+    });
 
-    // page2 = createButton('nonono');
-    // page2.mousePressed(function(){
-    //     classifier.addImage('nonono');
-    // });
+    page2 = createButton('nonono');
+    page2.mousePressed(function(){
+        classifier.addImage('nonono');
+    });
 
-    // page3 = createButton('leave me alone');
-    // page3.mousePressed(function(){
-    //     classifier.addImage('leave me alone');
-    // });
+    page3 = createButton('leave me alone');
+    page3.mousePressed(function(){
+        classifier.addImage('leave me alone');
+    });
 
-    // page4 = createButton('dont look');
-    // page4.mousePressed(function(){
-    //     classifier.addImage('dont look');
-    // });
+    page4 = createButton('dont look');
+    page4.mousePressed(function(){
+        classifier.addImage('dont look');
+    });
 
-    // page5 = createButton('help me');
-    // page5.mousePressed(function(){
-    //     classifier.addImage('help me');
-    // });
+    page5 = createButton('help me');
+    page5.mousePressed(function(){
+        classifier.addImage('help me');
+    });
 
-    // page6 = createButton('follows');
-    // page6.mousePressed(function(){
-    //     classifier.addImage('follows');
-    // });
+    page6 = createButton('follows');
+    page6.mousePressed(function(){
+        classifier.addImage('follows');
+    });
 
-    // page7 = createButton('cant run');
-    // page7.mousePressed(function(){
-    //     classifier.addImage('cant run');
-    // });
+    page7 = createButton('cant run');
+    page7.mousePressed(function(){
+        classifier.addImage('cant run');
+    });
 
-    // page8 = createButton('forest');
-    // page8.mousePressed(function(){
-    //     classifier.addImage('forest');
-    // });
+    page8 = createButton('forest');
+    page8.mousePressed(function(){
+        classifier.addImage('forest');
+    });
 
-    // /////// to train and save
+    /////// to train and save
 
-    // trainButton = createButton('train');
-    // trainButton.mousePressed(function(){
-    //     classifier.train(whileTraining);
-    // });
+    trainButton = createButton('train');
+    trainButton.mousePressed(function(){
+        classifier.train(whileTraining);
+    });
 
-    // saveButton = createButton('save');
-    // saveButton.mousePressed(function(){
-    //     classifier.save();
-    // });
+    saveButton = createButton('save');
+    saveButton.mousePressed(function(){
+        classifier.save();
+    });
 }
 
 function draw() {
     background(255, 0, 0);
     image(video_trained, 0, 0, video_trained.width, video_trained.height); //video on canvas, position, dimensions
+    fill(0);
+    rect(20, 400, 100, 20);
+    fill(255)
+    text(label, 20, 400, 300, 300);
 }
