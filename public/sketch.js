8c80ba079c4a7ad25958d5254b8edd8c54e566fd
fnassar <75119980+fnassar@users.noreply.github.com>
2022-05-11 22:38:05 +0400

    .

--- public/sketch.js
+++ public/sketch.js
@@ -46,8 +46,7 @@ let current_page;
 function gotResults(error, results) {
     if (error) {
         console.error(error);
-    }
-    else if (results[0].confidence>0.90){
+    } else if (results[0].confidence > 0.90) {
         label = results[0].label
     } else {
         label = "no object found"
@@ -138,7 +137,7 @@ function draw() {
     if (gameState == "start") {
         //background(255, 0, 0);
         image(wallpaper, 0, 0, width, height);
-        image(video, 0, 0, video.width*height/video.height, height); //video on canvas, position, dimensions
+        image(video, 0, 0, video.width * height / video.height, height); //video on canvas, position, dimensions
         //image(video, 0, 0, width, height); //video on canvas, position, dimensions
         image(effect, 0, 0, width, height)
 
@@ -153,114 +152,115 @@ function draw() {
         //["always watches","nonono","leave me alone","dont look","help me","follows","cant run","forest"]}}
 
         displayHints();
-        console.log(rooms[myname]);
+        // console.log(rooms[myname]);
         if (label != 'no object found') { console.log(level, label); }
 
 
 
-        if(level == 1 && label == "forest") {
-
-            let img_h = forest.height * (width-80) / forest.width
-            let y = (height-img_h)/2;
-            image(forest, 40, y, width-80, img_h);
+        if (level == 1 && label == "forest") {
+            level++;
+            let img_h = forest.height * (width - 80) / forest.width
+            let y = (height - img_h) / 2;
+            image(forest, 40, y, width - 80, img_h);
 
             rooms[myname].score++;
             let data = {
+                name: myname,
                 gameLevel: level,
                 score: rooms[myname].score
             };
             socket.emit('newWin', data);
-        } 
-        else if (level == 2 && label == "nonono") {
-
-            let img_h = nonono.height * (width-80) / nonono.width
-            let y = (height-img_h)/2;
-            image(nonono, 40, y, width-80, img_h);
-
-            rooms[myname].score++;
-            let data = {
-                gameLevel: level,
-                score: rooms[myname].score
-            };
-            socket.emit('newWin', data);
-
-        } 
-        else if (level == 3 && label == "leave me alone") {
-
-            let img_h = leave_me_alone.height * (width-80) / leave_me_alone.width
-            let y = (height-img_h)/2;
-            image(leave_me_alone, 40, y, width-80, img_h);
+        } else if (level == 2 && label == "nonono") {
+            level++;
+            let img_h = nonono.height * (width - 80) / nonono.width
+            let y = (height - img_h) / 2;
+            image(nonono, 40, y, width - 80, img_h);
 
             rooms[myname].score++;
             let data = {
+                name: myname,
                 gameLevel: level,
                 score: rooms[myname].score
             };
             socket.emit('newWin', data);
 
-        } 
-        else if (level == 4 && label == "dont look") {
-
-            let img_h = dont_look.height * (width-80) / dont_look.width
-            let y = (height-img_h)/2;
-            image(dont_look, 40, y, width-80, img_h);
+        } else if (level == 3 && label == "leave me alone") {
+            level++;
+            let img_h = leave_me_alone.height * (width - 80) / leave_me_alone.width
+            let y = (height - img_h) / 2;
+            image(leave_me_alone, 40, y, width - 80, img_h);
 
             rooms[myname].score++;
             let data = {
+                name: myname,
                 gameLevel: level,
                 score: rooms[myname].score
             };
             socket.emit('newWin', data);
 
-        } 
-        else if (level == 5 && label == "help me") {
-
-            let img_h = help_me.height * (width-80) / help_me.width
-            let y = (height-img_h)/2;
-            image(help_me, 40, y, width-80, img_h);
+        } else if (level == 4 && label == "dont look") {
+            level++;
+            let img_h = dont_look.height * (width - 80) / dont_look.width
+            let y = (height - img_h) / 2;
+            image(dont_look, 40, y, width - 80, img_h);
 
             rooms[myname].score++;
             let data = {
+                name: myname,
                 gameLevel: level,
                 score: rooms[myname].score
             };
             socket.emit('newWin', data);
 
-        } 
-        else if (level == 6 && label == "follows") {
-
-            let img_h = follows.height * (width-80) / follows.width
-            let y = (height-img_h)/2;
-            image(follows, 40, y, width-80, img_h);
+        } else if (level == 5 && label == "help me") {
+            level++;
+            let img_h = help_me.height * (width - 80) / help_me.width
+            let y = (height - img_h) / 2;
+            image(help_me, 40, y, width - 80, img_h);
 
             rooms[myname].score++;
             let data = {
+                name: myname,
                 gameLevel: level,
                 score: rooms[myname].score
             };
             socket.emit('newWin', data);
-        } 
-        else if (level == 7 && label == "cant run") {
 
-            let img_h = cant_run.height * (width-80) / cant_run.width
-            let y = (height-img_h)/2;
-            image(cant_run, 40, y, width-80, img_h);
+        } else if (level == 6 && label == "follows") {
+            level++;
+            let img_h = follows.height * (width - 80) / follows.width
+            let y = (height - img_h) / 2;
+            image(follows, 40, y, width - 80, img_h);
 
             rooms[myname].score++;
             let data = {
+                name: myname,
                 gameLevel: level,
                 score: rooms[myname].score
             };
             socket.emit('newWin', data);
-        } 
-        else if (level == 8 && label == "always watches") {
-
-            let img_h = always_watches.height * (width-80) / always_watches.width
-            let y = (height-img_h)/2;
-            image(always_watches, 40, y, width-80, img_h);
+        } else if (level == 7 && label == "cant run") {
+            level++;
+            let img_h = cant_run.height * (width - 80) / cant_run.width
+            let y = (height - img_h) / 2;
+            image(cant_run, 40, y, width - 80, img_h);
 
             rooms[myname].score++;
             let data = {
+                name: myname,
+                gameLevel: level,
+                score: rooms[myname].score
+            };
+            socket.emit('newWin', data);
+        } else if (level == 8 && label == "always watches") {
+
+            let img_h = always_watches.height * (width - 80) / always_watches.width
+            let y = (height - img_h) / 2;
+            image(always_watches, 40, y, width - 80, img_h);
+
+            rooms[myname].score++;
+            let data = {
+                name: myname,
                 gameLevel: level,
                 score: rooms[myname].score
             };
@@ -273,9 +273,9 @@ function draw() {
         textAlign(CENTER, TOP);
         textSize(50);
         textFont(font);
-        text("instructions", width/2, 212);
+        text("instructions", width / 2, 212);
         textSize(20);
-        text("touch anywere to continue", width/2, height-200);
+        text("touch anywere to continue", width / 2, height - 200);
 
 
     } else if (gameState == "help") {
@@ -284,8 +284,8 @@ function draw() {
         image(button_return, button_x, button_y, button_r, button_r);
         //textSize(50);
         textFont(font);
-        text("insert message here", width/2, height-50);
-        
+        text("insert message here", width / 2, height - 50);
+
     }
 
 }
@@ -313,8 +313,8 @@ function touchStarted() {
 
 function displayHints() {
 
-    let box_height = hint_box.height*width/hint_box.width
-    image(hint_box, 0, height-box_height/1.5, width, box_height);
+    let box_height = hint_box.height * width / hint_box.width
+    image(hint_box, 0, height - box_height / 1.5, width, box_height);
     let hint;
 
     if (current_page == 1) {
@@ -338,6 +338,6 @@ function displayHints() {
     textFont(font);
     textSize(20);
     textAlign(CENTER, TOP);
-    text(hint, width/2, height-box_height/2);
+    text(hint, width / 2, height - box_height / 2);
 
 }
