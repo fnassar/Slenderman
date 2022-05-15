dc09ffba2c1817cd4298b008ad37d3360fad10ed
andresugartechea <78426894+andresugartechea@users.noreply.github.com>
2022-05-15 15:40:13 +0400

    min details

--- public/training.js
+++ public/training.js
@@ -1,18 +1,11 @@
-// // Your code will go here
-// // open up your console - if everything loaded properly you should see the latest ml5 version
-// //console.log('ml5 version:', ml5.version)
+//This code is based on The Coding Train's tutorial on feature extractor classification: https://thecodingtrain.com/learning/ml5/3.1-feature-extractor-classification.html
 
-
-//To change the camera https://editor.p5js.org/AndreasRef/sketches/HJVgGjmz4
-
-
-
-
+//For the detector and model trainer
 let video_trained;
 let mobilenet_1;
 let classifier;
 
-//To train each item
+//Buttons train each item
 let page1;
 let page2;
 let page3;
@@ -21,12 +14,11 @@ let page5;
 let page6;
 let page7;
 let page8;
-
 let trainButton;
 let saveButton; 
 
+//To read the objects detected and the accuracy
 let label = "loading model"
-
 let confidence_value = 0.95;
 
 
@@ -34,7 +26,6 @@ function gotResults(error, results){
     if (error) {
         console.error(error);
     }
-    //console.log(results)
     if (results[0].confidence>confidence_value){
         label = results[0].label + " " + confidence_value
     } else {
@@ -45,7 +36,7 @@ function gotResults(error, results){
 
 function modelReady() {
     console.log("model is ready")
-    classifier.load("/models/model.json", customModelReady);
+    classifier.load("/models/model.json", customModelReady); //uses trained model
 }
 
 function customModelReady() {
@@ -60,9 +51,8 @@ function videoReady() {
 
 function whileTraining(loss){
     if(loss == null) {
-        //console.log("training complete");
         fill(255);
-        document.getElementById("title").innerHTML = "training complete"
+        document.getElementById("title").innerHTML = "training complete" //notifies the user when the model has been trained
         classifier.classify(gotResults)
     }
 }
@@ -76,7 +66,6 @@ function setup() {
     //to set up the front camera on the phone
     var constraints = {
         audio: false,
-
         ///TO TEST ON PHONE, COMMENT IN THIS PART AND COMMENT OUT THE OTHER ONE 
         // video: {
         //     facingMode: {
@@ -87,16 +76,14 @@ function setup() {
           facingMode: "user"
         } 
     };
-
     video_trained = createCapture(constraints);
     video_trained.hide();
 
-    //COCOSD
+    //For classifier
     mobilenet = ml5.featureExtractor('MobileNet', modelReady);
     classifier = mobilenet.classification(video_trained, {numLabels: 8} , videoReady);
 
-
-    //To train ML5
+    //Buttons to train ML5
     page1 = createButton('always watches');
     page1.mousePressed(function(){
         classifier.addImage('always watches');
@@ -137,8 +124,8 @@ function setup() {
         classifier.addImage('forest');
     });
 
-    /////// to train and save
 
+    //To finish training and save model
     trainButton = createButton('train');
     trainButton.mousePressed(function(){
         classifier.train(whileTraining);
@@ -153,16 +140,18 @@ function setup() {
 function draw() {
     background(255, 0, 0);
     image(video_trained, 0, 0, video_trained.width, video_trained.height); //video on canvas, position, dimensions
+
+    //rectangles are drawn as background for the texts, to read them easier
     fill(0);
     rect(20, 400, 100, 20);
     fill(255)
     text(label, 20, 400, 300, 300);
-
     rect(0, height-50, 50, 50);
     rect(width-50, height-50, 50, 50);
 }
 
 function touchStarted(){
+    //Used to create 'buttons' to test different confident_values
     if ((mouseX<50)&&(mouseX>0)&&(mouseY<height)&&(mouseY>height-50)){
         confidence_value -= 0.01;
     }
